24b5a97a20d8485b9173c2e49125e7a4bacb4233
francisco-q <quevedof98@gmail.com>
2024-12-19 02:56:57 -0300

    corsheaders \ rest_framework

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class SafeSpace(models.Model):
+    title = models.CharField(max_length=50)
+    horario = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    xCoord = models.FloatField()
+    yCoord = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    

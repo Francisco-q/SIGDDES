9a298a6e2800da5b27ecfa5c0910c889eae25195
francisco-q <quevedof98@gmail.com>
2025-01-09 17:03:35 -0300

    Swagger v.1 - Front sin conexión

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'drf_yasg',
     'tasks',
 ]
 
@@ -119,8 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -128,3 +130,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS AUTORIZACIONES
 CORS_ALLOWED_ORIGINS = []
+
--- django_crud_api/urls.py
+++ django_crud_api/urls.py
@@ -16,11 +16,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from tasks.views import home
+from rest_framework import routers
+from tasks.views import home, TaskViewSet
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
+router = routers.DefaultRouter()
+router.register(r'tasks', TaskViewSet, 'task')
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Tasks API",
+        default_version='v1',
+        description="RESTful API for Tasks",
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
     path('tasks/', include('tasks.urls')),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     
 ]

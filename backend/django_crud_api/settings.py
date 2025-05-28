51e974007c0e25a5f34829e1b0870fac83064a3b
francisco-q <quevedof98@gmail.com>
2025-05-28 02:04:58 -0400

    upd

--- backend/django_crud_api/settings.py
+++ backend/django_crud_api/settings.py
@@ -9,7 +9,7 @@ from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost')
 
